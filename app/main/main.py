72f215f3823cff863d51a3317b6630b2055259db
imatsoup <93290208+imatsoup@users.noreply.github.com>
2024-12-31 01:06:05 -0500

    small cleanup

--- app/main/main.py
+++ app/main/main.py
@@ -7,7 +7,7 @@ import sys
 
 
 app = Flask(__name__)
-app.secret_key = "anuvs5flz13zlnsat1"
+app.secret_key = 'anuvs5flz13zlnsat1'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.permanent_session_lifetime = timedelta(minutes=5)
@@ -15,7 +15,7 @@ app.permanent_session_lifetime = timedelta(minutes=5)
 db = SQLAlchemy(app)
 
 class users(db.Model):
-    _id = db.Column("id", db.Integer, primary_key=True)
+    _id = db.Column('id', db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(100))
     pw = db.Column(db.String(100))
@@ -25,35 +25,35 @@ class users(db.Model):
         self.email = email
         self.pw = pw
 
-@app.route("/")
+@app.route('/')
 def test():
-    return "<h1>Test</h1>"
+    return '<h1>Test</h1>'
 
-@app.route("/home", methods=["POST", "GET"])
+@app.route('/home', methods=['POST', 'GET'])
 def home():
     # IF METHOD POST, WRITE TO FILE, then do the rest and display our new text
     filepath= Path('text.txt')
-    text = "Default"
+    text = 'Default'
     if filepath.is_file():
         with open('text.txt', 'r') as f:
             text = f.read();
-    # blog = request.form["text"]
-    return render_template("index.html", text = text)
-@app.route("/create", methods=["POST", "GET"])
+    # blog = request.form['text']
+    return render_template('index.html', text = text)
+@app.route('/create', methods=['POST', 'GET'])
 def create():
-    if request.method == "POST":
+    if request.method == 'POST':
         session.permanent = True
-        uname = request.form["nm"]
-        pw = request.form["pw"]
-        uemail = request.form["email"]
+        uname = request.form['nm']
+        pw = request.form['pw']
+        uemail = request.form['email']
         name_found = users.query.filter_by(name=uname).first()
         email_found = users.query.filter_by(email=uemail).first()
         if name_found:
-            flash("Username already in use", "info")
-            return redirect(url_for("create"))
+            flash('Username already in use', 'info')
+            return redirect(url_for('create'))
         if email_found:
-            flash("Email already in use!", "info")
-            return redirect(url_for("create"))
+            flash('Email already in use!', 'info')
+            return redirect(url_for('create'))
         else:
             bytes = pw.encode()
             salt = bcrypt.gensalt()
@@ -61,32 +61,32 @@ def create():
             new_usr = users(uname, uemail, hashed_password)
             db.session.add(new_usr)
             db.session.commit()
-            flash("Account created!")
-            return redirect(url_for("login"))
+            flash('Account created!')
+            return redirect(url_for('login'))
     else:
-        return render_template("create.html")
-@app.route("/login", methods=["POST", "GET"])
+        return render_template('create.html')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-        if request.method == "POST":       
-            uname = request.form["nm"]
-            pw = request.form["pw"]
+        if request.method == 'POST':       
+            uname = request.form['nm']
+            pw = request.form['pw']
             found_user = users.query.filter_by(name=uname).first()
             if found_user and bcrypt.checkpw(pw.encode(), found_user.pw):
                 session.permanent = True
-                session["user"] = uname
-                flash("Identity confirmed", "info")
-                return redirect(url_for("home"))
+                session['user'] = uname
+                flash('Identity confirmed', 'info')
+                return redirect(url_for('home'))
             else: 
-                flash("Your information was incorrect!", "info")
-                return redirect(url_for("login"))
+                flash('Your information was incorrect!', 'info')
+                return redirect(url_for('login'))
         else:
-            return render_template("login.html")
-@app.route("/logout")
+            return render_template('login.html')
+@app.route('/logout')
 def logout():
-    flash("You have been logged out!", "info")
-    session.pop("user", None)
-    return redirect(url_for("login"))
-if __name__ == "__main__":
+    flash('You have been logged out!', 'info')
+    session.pop('user', None)
+    return redirect(url_for('login'))
+if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         admin_pw = '123'
